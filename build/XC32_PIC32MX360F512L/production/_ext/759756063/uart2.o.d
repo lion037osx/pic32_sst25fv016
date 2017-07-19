8d29e51388f4238897abbf279ccc8c13dab0039d
optimus <optimus@optimusPresario21>
2017-07-19 10:49:01 -0300

    update_3

---
+++ build/XC32_PIC32MX360F512L/production/_ext/759756063/uart2.o.d
@@ -0,0 +1,6 @@
+build/XC32_PIC32MX360F512L/production/_ext/759756063/uart2.o:  \
+ /home/optimus/Documentos/Microchip/firmware/firmware_pic32/prjt_pic32_sst25fv/pic32_sst25fv_spi.X/uart2.c  \
+ /home/optimus/Documentos/Microchip/firmware/firmware_pic32/prjt_pic32_sst25fv/pic32_sst25fv_spi.X/Compiler.h  \
+ /home/optimus/Documentos/Microchip/firmware/firmware_pic32/prjt_pic32_sst25fv/pic32_sst25fv_spi.X/HardwareProfile.h  \
+ /home/optimus/Documentos/Microchip/firmware/firmware_pic32/prjt_pic32_sst25fv/pic32_sst25fv_spi.X/system.h  \
+ /home/optimus/Documentos/Microchip/firmware/firmware_pic32/prjt_pic32_sst25fv/pic32_sst25fv_spi.X/uart2.h 
